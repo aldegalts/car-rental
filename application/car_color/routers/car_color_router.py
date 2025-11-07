0ff5ce975c81660b4860b907e8fd1558a69af1ca
aldegalts <alinadegalceva@mail.ru>
2025-11-07 18:12:14 +0300

    feat: add car color management

---
+++ application/car_color/routers/car_color_router.py
@@ -0,0 +1,54 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+
+from application.car_color.schemas import CarColorRead, CarColorCreate, CarColorUpdate
+from application.car_color.usecases import CreateCarColorUseCase, DeleteCarColorUseCase, GetAllCarColorsUseCase, \
+    UpdateCarColorUseCase
+from application.dependencies import get_current_user
+from infrastructure.database.database_session import get_db
+
+router = APIRouter(prefix="/car_colors", tags=["Car Colors"])
+
+
+@router.post("/", response_model=CarColorRead, status_code=status.HTTP_201_CREATED)
+def add_color(
+    color_data: CarColorCreate,
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user)
+):
+    if current_user.role.role_name != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only admin can create colors")
+
+    return CreateCarColorUseCase(db).execute(color_data)
+
+
+@router.delete("/{color_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_color(
+    color_id: int,
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user)
+):
+    if current_user.role.role_name != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only admin can delete colors")
+
+    DeleteCarColorUseCase(db).execute(color_id)
+    return {"detail": "Color deleted successfully"}
+
+
+@router.get("/", response_model=List[CarColorRead])
+def get_all_colors(db: Session = Depends(get_db)):
+    return GetAllCarColorsUseCase(db).execute()
+
+
+@router.put("/{color_id}", response_model=CarColorRead)
+def add_color(
+    color_data: CarColorUpdate,
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user)
+):
+    if current_user.role.role_name != "admin":
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Only admin can update colors")
+
+    return UpdateCarColorUseCase(db).execute(color_data)
---
+++ application/dependencies.py
@@ -0,0 +1,38 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
+
+from application.auth.utils.jwt import decode_token
+from infrastructure.database.repository import UserRepository
+from infrastructure.database.database_session import get_db
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
+    payload = decode_token(token)
+    if not payload:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token",
+        )
+
+    user_id = payload.get("sub")
+    if not user_id:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token payload",
+        )
+
+    user_repo = UserRepository(db)
+    user = user_repo.get_by_id(user_id)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found",
+        )
+
+    return user
