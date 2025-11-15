d7989c1e965a12daad5f500778fb4e5f322154f2
aldegalts <alinadegalceva@mail.ru>
2025-11-15 22:54:23 +0300

    feat: add logout

--- application/auth/routers/auth.py
+++ application/auth/routers/auth.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, status, Response
+from fastapi import APIRouter, Depends, status, Response, Cookie
 from sqlalchemy.orm import Session
 from infrastructure.database.database_session import get_db
 from application.auth.schemas import UserCreate, UserLogin, UserResponse, RefreshRequest, AccessTokenResponse
-from application.auth.usecases import RegisterUserUseCase, LoginUserUseCase, RefreshAccessTokenUseCase
+from application.auth.usecases import RegisterUserUseCase, LoginUserUseCase, RefreshAccessTokenUseCase, LogoutUseCase
 from fastapi.templating import Jinja2Templates
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
@@ -22,3 +22,12 @@ def login_user(login_data: UserLogin, response: Response, db: Session = Depends(
 @router.post("/refresh", response_model=AccessTokenResponse)
 def refresh_access_token(request: RefreshRequest, db: Session = Depends(get_db)):
     return RefreshAccessTokenUseCase(db).refresh_access_token(request)
+
+
+@router.post("/logout")
+def logout(
+    response: Response,
+    db: Session = Depends(get_db),
+    refresh_token: str = Cookie(default=None)
+):
+    return LogoutUseCase(db).execute(refresh_token, response)
--- infrastructure/database/repository/refresh_token_repository.py
+++ infrastructure/database/repository/refresh_token_repository.py
@@ -10,3 +10,13 @@ class RefreshTokenRepository:
     def add(self, token: RefreshTokenEntity):
         self.session.add(token)
         self.session.commit()
+
+    def delete(self, token: str):
+        token_obj = (
+            self.session.query(RefreshTokenEntity)
+            .filter(RefreshTokenEntity.token == token).first()
+        )
+        
+        if token_obj:
+            self.session.delete(token_obj)
+            self.session.commit()
